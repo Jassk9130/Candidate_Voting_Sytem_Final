92d4849cc6a611669ebddbac44607a70bc607d5a
Jaskaran <Jassk9130@gmail.com>
2021-02-28 13:42:33 +1300

    Add project files.

---
+++ Migrations/Candidate_Voting_Data/20210228081420_Data.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace Candidate_Voting_Sytem_Final.Migrations.Candidate_Voting_Data
+{
+    public partial class Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CandidateProfileWebSite = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vote",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VoterId = table.Column<int>(type: "int", nullable: false),
+                    CandidateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vote", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vote_Candidate_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vote_Voter_VoterId",
+                        column: x => x.VoterId,
+                        principalTable: "Voter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vote_CandidateId",
+                table: "Vote",
+                column: "CandidateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vote_VoterId",
+                table: "Vote",
+                column: "VoterId");
+
+            var sqlFile = Path.Combine(".\\DatabaseScripts", @"data.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vote");
+
+            migrationBuilder.DropTable(
+                name: "Candidate");
+
+            migrationBuilder.DropTable(
+                name: "Voter");
+        }
+    }
+}
